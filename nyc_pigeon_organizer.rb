787c9bcbc81b80de26c10581db1021d3a45332c1
Jason Leach <jlspursfan@gmail.com>
2019-05-27 02:15:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,18 +5,39 @@ def nyc_pigeon_organizer(data)
   data.each do |trait, trait_hash|
     trait_hash.each do |k, arr|
       arr.each do |name|
-        new_arr = []
-        if (trait_hash[k]).include?(name)
-          new_arr << k.to_s
-        end
-
-        new_trait_hash[trait] = new_arr.uniq
-
-        if !new_hash.has_key?(name)
-          new_hash[name] = new_trait_hash
-        end
+        new_hash[name] = { :color => [], :gender => [], :lives => [] }
       end
     end
   end
+
+  x = new_hash.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_hash[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_hash[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_hash[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
   return new_hash
 end
