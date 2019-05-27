a209bd76c7a88ecd9678311c781a37897f676c79
Jason Leach <jlspursfan@gmail.com>
2019-05-27 02:15:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,28 +16,28 @@ def nyc_pigeon_organizer(data)
       x.each do |item|
         if bird_name === item
           new_hash[item][:color] << bird_color.to_s
-        end 
-      end 
-    end 
-  end 
+        end
+      end
+    end
+  end
   data[:gender].each do |gender, type|
     type.each do |bird_name|
       x.each do |item|
         if bird_name === item
           new_hash[item][:gender] << gender.to_s
-        end 
-      end 
-    end 
-  end 
+        end
+      end
+    end
+  end
   data[:lives].each do |location, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
           new_hash[item][:lives] << location
-        end 
-      end 
-    end 
-  end 
-  
+        end
+      end
+    end
+  end
+
   return new_hash
 end
